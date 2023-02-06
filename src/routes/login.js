0fadfe713684799c31532bc132810fa19b33f482
DennisMugarte <gameplaysdennis@gmail.com>
2023-02-05 20:08:34 -0600

    register__loginController

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const LoginController = require('../controllers/LogiinController');
+const LoginController = require('../controllers/LoginController');
 
 const router = espress.Router();
 
-router.get('/login', LoginController.login);
+router.get('/login', LoginController.index);
 router.get('/register', LoginController.register);
+router.post('/auth', LoginController.auth);
+router.get('/logout', LoginController.logout);
 
 module.exports = router;
